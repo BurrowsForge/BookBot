09b78d18320de5041f518ce8344f2a87110b4cae
BurrowsForge <155598491+BurrowsForge@users.noreply.github.com>
2025-12-11 17:49:42 -0500

    Updated with stats.py for refactoring.

--- main.py
+++ main.py
@@ -1,2 +1,13 @@
-print("greetings boots")
+from stats import get_num_words
+
+def get_book_text(path):
+    with open(path) as f:
+        return f.read()
+
+def main():
+    text = get_book_text("books/frankenstein.txt")
+    count = get_num_words(text)
+    print(f"Found {count} total words")
+        
+main()
       
---
+++ stats.py
@@ -0,0 +1,4 @@
+def get_num_words(text):
+    num_words = len(text.split())
+    return num_words
+   
